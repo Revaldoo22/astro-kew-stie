0f6af94cd3a06cf0c5aa384960286bcf24190e45
EKO HARY <62006035+ehary-devs@users.noreply.github.com>
2026-05-04 15:57:41 +0700

    initial commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-    apps: [
-      {
-        name: 'server',
-        script: 'server.js',
-        exec_mode: 'cluster',
-        instances: 'max',
-        env: {
-          NODE_ENV: 'production',
-          PORT: 30069,
-        },
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
+  apps: [
+    {
+      name: 'server',
+      script: 'server.js',
+      exec_mode: 'cluster',
+      instances: 'max',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 30069,
       },
-    ],
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+    },
+  ],
 };
-  
+
